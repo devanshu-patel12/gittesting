204b78f7e4dff6574b54c7d5c647e478629c6b9f
devanshu-patel12 <devanshu.patel@rapidops.co>
2024-05-21 17:13:34 +0530

    tried git revert

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 if (1 === 1) {
   console.log("true");
 } else console.log("false");
-if(1){
-  console.log('one',"two","three");
+if (1) {
+  console.log("one");
 }
-function addSum(){
-  console.log(1+2);
-}
-console.log(addSum());
