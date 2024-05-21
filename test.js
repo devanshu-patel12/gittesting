ce340a7fb0d914ae9b481838ef17fd9e8b5aff6f
priyanka.parekh <priyanka.parekh@rapidops.co>
2024-05-21 14:53:57 +0530

    four in same line

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ if (1 === 1) {
   console.log("true");
 } else console.log("false");
 if(1){
-  console.log('one',"two","three");
+  console.log('one',"two","three", 'four');
 }
 function addSum(){
   console.log(1+2);
