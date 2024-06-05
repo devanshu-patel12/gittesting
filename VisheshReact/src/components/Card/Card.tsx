f20f92ac20829bd49034d35b7efd17b267c70cad
VemulaKarthik <karthik.vemula@rapidops.co>
2024-06-05 17:42:31 +0530

    A new line is added

--- VisheshReact/src/components/Card/Card.tsx
+++ VisheshReact/src/components/Card/Card.tsx
@@ -1,48 +1,24 @@
-import "./Card.css";
+import './Card.css'
 type CardPropsType = {
-    name: string;
-    age: number;
-    productName: string;
-    productPrice: number;
-};
-function Card({ name, age, productName, productPrice }: CardPropsType) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+    name: string
+    age: number
+    productName: string
+    productPrice: number
+}
+function Card({ name, age, productName, productPrice, title }: CardPropsType) {
 
     return (
         <>
             <div className="card">
+                <p>Title : {title}</p>
                 <h5>Name : {name}</h5>
-=======
-    return (
-        <>
-            <div className="card">
-                <p>Name and Surname : {name}</p>
->>>>>>> a9fc1127a51bd4f2d84601fd0e96dc8900e985e4
                 <h3>Age : {age}</h3>
                 <h2>Product Chosen: {productName}</h2>
                 <h3>It's Price: {productPrice}</h3>
             </div>
-<<<<<<< HEAD
 
         </>
     )
-=======
-  return (
-    <>
-      <div className="card">
-        <p>Name and Surname : {name}</p>
-        <h3>Age : {age}</h3>
-        <h2>Product Chosen: {productName}</h2>
-        <h3>It's Price: {productPrice}</h3>
-      </div>
-    </>
-  );
->>>>>>> 4d44309c48a401b54c454af2ae9bfcc43d074572
-=======
-        </>
-    );
->>>>>>> a9fc1127a51bd4f2d84601fd0e96dc8900e985e4
 }
 
-export default Card;
+export default Card
