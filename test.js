36e1c4af58be28952849aa807ab10754f0ed6e44
vishesh-shrivastava <vishesh.shrivastava@rapidops.co>
2024-06-05 11:18:36 +0530

    Conflict resolved 11:40 Yes

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 if (1 === 1) {
   console.log("true");
 } else console.log("false");
+if(1){
+
+  console.log('one',"two","three","four","five","six","seven","eight");
+}
+function addSum(){
+  console.log(1+2);
 if (1) {
   console.log("one", "two", "three");
 }
@@ -8,3 +14,4 @@ function addSum() {
   console.log(1 + 2);
 }
 console.log(addSum());
+}
