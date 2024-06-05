a9fc1127a51bd4f2d84601fd0e96dc8900e985e4
vishesh-shrivastava <vishesh.shrivastava@rapidops.co>
2024-06-05 12:29:57 +0530

    New Commit WIth Skip after rebase

--- VisheshReact/src/components/Card/Card.tsx
+++ VisheshReact/src/components/Card/Card.tsx
@@ -1,21 +1,21 @@
 import "./Card.css";
 type CardPropsType = {
-  name: string;
-  age: number;
-  productName: string;
-  productPrice: number;
+    name: string;
+    age: number;
+    productName: string;
+    productPrice: number;
 };
 function Card({ name, age, productName, productPrice }: CardPropsType) {
-  return (
-    <>
-      <div className="card">
-        <p>Name and Surname : {name}</p>
-        <h3>Age : {age}</h3>
-        <h2>Product Chosen: {productName}</h2>
-        <h3>It's Price: {productPrice}</h3>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="card">
+                <p>Name and Surname : {name}</p>
+                <h3>Age : {age}</h3>
+                <h2>Product Chosen: {productName}</h2>
+                <h3>It's Price: {productPrice}</h3>
+            </div>
+        </>
+    );
 }
 
 export default Card;
